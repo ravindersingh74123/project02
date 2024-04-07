361fb6455c818098ade53d165d0cac0bceac72ec
ravindersingh74123 <154728708+ravindersingh74123@users.noreply.github.com>
2024-04-08 00:38:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -231,6 +231,7 @@ io.on("connection", (socket) => {
 });
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use('/images', express.static(path.join(__dirname, 'frontend', 'dist', 'images')));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
